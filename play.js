3e66ee012c121e9e66a516835aaacb1b30db1a1f
Sylas Serné <its.sylas@gmail.com>
2021-06-30 23:56:38 +0000

    now we can quit :)

--- play.js
+++ play.js
@@ -2,8 +2,24 @@
 // const connect = client.connect;
 const {connect} = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
 // establishes a connection with the game server
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
